39ef43831f065148b2b8787aab1150b490a5f823
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2018-10-28 19:50:53 -0400

    added users model to the blogging app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts do
     resources :comments
+    resources :users
   end
   root "posts#index"
 end
