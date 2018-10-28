f7a37da78e032d91192bbc6d4bb38164bc5c4306
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2018-10-28 18:48:39 -0400

    added db:seed to stage dummy data on first time app setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+posts = Post.create([{title: 'A db:seed generated magic post', content: 'This magical post comes from rails db:seed',
+                      created_at: "01/01/98 23:59:59.999", updated_at: "01/01/98 23:59:59.999"}])
+
